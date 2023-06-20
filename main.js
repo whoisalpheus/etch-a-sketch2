e8098b3113d2aac92e070af2a88b1075f6b9a369
whoisalpheus <104221606+whoisalpheus@users.noreply.github.com>
2023-06-20 13:00:10 -0400

    Click event is not defining gridNum, how do I do this?

--- main.js
+++ main.js
@@ -3,30 +3,40 @@ const box = document.querySelector('.box');
 const controls = document.querySelector('.controls');
 const dropMenu = document.querySelector('#grid-size');
 
-// Grid
-function createGrid() {
-    for (let i = 0; i < 16; i++) {
-        const row = document.createElement('div');
-        row.classList.add('row');
-        box.appendChild(row);
-        for (let j = 0; j < 16; j++) {
-            const column = document.createElement('div');
-            column.classList.add('column');
-            row.appendChild(column);                
-        }                
-    }
-} createGrid();
-
-// Size of grid
-// Label: 'Choose size of canvas'
-// Select: up to 100 options...
+// Creates 1-100 in drop down menu <option>
 for (let i = 1; i < 101; i++) {
     const sizeSelection = document.createElement('option');
     sizeSelection.textContent = i;
     dropMenu.appendChild(sizeSelection);
 }
 
+// Grid
+function createGrid() {
+    let gridNum = dropMenu.addEventListener('click', gridSize());
+    for (let i = 0; i < gridNum; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        box.appendChild(row);
+        for (let j = 0; j < gridNum; j++) {
+            const column = document.createElement('div');
+            column.classList.add('column');
+            row.appendChild(column);                
+        }                
+    }
+}
+
+// Size of grid
+// Label: 'Choose size of canvas'
+// Select: up to 100 options...
+
+// When I click option #, it should go to gridNum:
+// dropMenu.addEventListener('click', gridSize());
+
+function gridSize() { 
+    return dropMenu.textContent;        
+} console.log(gridSize());
+
 // Change size of grid
-.addEventListener('click', () => {
-    
-})
+// sizeSelection.addEventListener('click', () => {
+//     createGrid();    
+// })
